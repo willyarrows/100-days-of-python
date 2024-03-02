6ff09cb52c0c02902d7b4c7c534b1284adbf4cfa
Willy Sebastian <willy.sebastian@gmail.com>
2024-03-02 15:12:12 +0800

    Adding Key Error exception

--- project026-nato-alphabet/main.py
+++ project026-nato-alphabet/main.py
@@ -3,7 +3,15 @@ import pandas as pd
 df_nato = pd.read_csv("nato_phonetic_alphabet.csv")
 dict_nato = {row.letter: row.code for (index, row) in df_nato.iterrows()}
 
-input_word = input("Enter a word : ").upper()
-nato_alphabet = [dict_nato[alphabet] for alphabet in input_word]
-print(nato_alphabet)
+is_exit = False
+while not is_exit:
+    input_word = input("Enter a word : ").upper()
+    try:
+        nato_alphabet = [dict_nato[alphabet] for alphabet in input_word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        print(nato_alphabet)
+        is_exit = True
+
 
