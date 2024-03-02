3fae25182356437f7b9d46df5d1d6b1bd2b8f2a6
Willy Sebastian <willy.sebastian@gmail.com>
2024-03-02 16:51:52 +0800

    Adding json version of password manager app

--- project030-password-manager-json-app/main.py
+++ project030-password-manager-json-app/main.py
@@ -4,7 +4,7 @@ import random
 import pyperclip
 import json
 
-DEFAULT_USERNAME = "willy.sebastian@gmail.com"
+DEFAULT_USERNAME = "xxx@gmail.com"
 LETTERS = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
            'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
            'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
